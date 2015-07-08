e847add4904f9104c7328f6b1b967054039a72d9
Alban Mouton <alban.mouton@gmail.com>
2015-07-08 09:38:29 +0200

    No need to go in console anymore.

--- live-blanket.js
+++ live-blanket.js
@@ -1,4 +1,13 @@
+/*global blanket*/
+
 (function() {
+	"use strict";
+
+	var elemDiv = document.createElement('div');
+	elemDiv.id = 'live-blanket';
+	elemDiv.style.cssText = 'position:absolute;z-index:100;background:white; top: 0px; border: 1px solid red;';
+	elemDiv.innerHTML = 'Live Blanket waiting for code instrumentation by blanket. You should probably do nothing in the meantime.';
+	document.body.appendChild(elemDiv);
 
 	window.liveBlanket = function() {
 		blanket.onTestDone(1, true);
@@ -9,6 +18,11 @@
 		callback: function() {
 			blanket.setupCoverage();
 			blanket.onTestStart();
+			elemDiv.innerHTML = 'Live Blanket OK. You should interact with the page, then ask for coverage results.';
+			var elemButton = document.createElement('button');
+			elemButton.innerHTML = 'click for results';
+			elemButton.onclick = window.liveBlanket;
+			elemDiv.appendChild(elemButton);
 		}
 	});
 })();
